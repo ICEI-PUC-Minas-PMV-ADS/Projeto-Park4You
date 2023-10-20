46dc3a6aedc2192d2443d36e710995f1335e2dd5
Alessandro Nunes Assuncao <nunes.1370451@gmail.com>
2023-10-20 08:53:55 -0300

    Alteração Index

---
+++ Projeto Park4You/Migrations/20231020115005_M01.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Projeto_Park4You.Migrations
+{
+    public partial class M01 : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
